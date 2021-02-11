4826c4e5e94e6312b56b01b9e52bd4129a1c76b9
fabba1892 <72152804+fabba1892@users.noreply.github.com>
2021-02-11 10:02:26 +0200

    code spotify token

--- src/spotify.js
+++ src/spotify.js
@@ -24,3 +24,5 @@ export const getTokenFromUrl = () => {
 }
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
+// code spotify token
+
